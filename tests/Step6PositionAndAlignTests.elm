36708a26a69f1d3da0ae7703fc248fb53a841d80
Martin Janiczek <martin@janiczek.cz>
2026-06-14 00:37:11 +0200

    Pave the way for Step 6 algo

--- tests/Step6PositionAndAlignTests.elm
+++ tests/Step6PositionAndAlignTests.elm
@@ -1,10 +1,41 @@
 module Step6PositionAndAlignTests exposing (suite)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer)
+import El exposing (..)
+import Expect
+import Fuzz
+import Log
+import Step0Annotate as Step0
+import Step1FitSizingAlong as Step1
+import Step2GrowShrinkSizingAlong as Step2
+import Step3WrapText as Step3 exposing (charHeight, charWidth)
+import Step4FitSizingAcross as Step4
+import Step5GrowShrinkSizingAcross as Step5
+import Step6PositionAndAlign as Step6
 import Test exposing (Test)
+import TestHelpers exposing (default)
+
+
+config : Config
+config =
+    { layoutWidth = 640
+    , layoutHeight = 480
+    }
+
+
+run : El -> AnnotatedEl
+run el =
+    el
+        |> Step0.annotate
+        |> Step1.fitSizingAlong
+        |> Step2.growShrinkSizingAlong config
+        |> Step3.wrapText
+        |> Step4.fitSizingAcross
+        |> Step5.growShrinkSizingAcross config
+        |> Step6.positionAndAlign
 
 
 suite : Test
 suite =
-    Test.todo "Implement tests"
+    Test.describe "Step6.positionAndAlign"
+        [ Test.todo "Some tests"
+        ]
